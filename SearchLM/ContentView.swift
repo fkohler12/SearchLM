0875279cd206a27742027053151b115d49fa9315
Felix Kohler <fkkohler@gmail.com>
2024-05-30 16:40:12 -0400

    Initial Commit

---
+++ SearchLM/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SearchLM
+//
+//  Created by Felix Kohler (student LM) on 5/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchLM/SearchLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchLMApp.swift
+//  SearchLM
+//
+//  Created by Felix Kohler (student LM) on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
